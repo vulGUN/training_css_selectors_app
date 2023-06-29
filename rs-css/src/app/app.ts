21a61d083a37fd791850682eb40d0ae399cbefdc
Max Lihachev <likha.max@gmail.com>
2023-06-29 15:38:16 +0300

    feat: add app.ts file with some start code

---
+++ rs-css/src/app/app.ts
@@ -0,0 +1,25 @@
+import { LayoutScreen } from '../components/layout-screen/layoutScreen';
+import { Levels } from '../components/levels/levels';
+import { Input } from '../components/input/input';
+import { CodeScreen } from '../components/code-screen/codeScreen';
+
+export class App {
+  private readonly layoutScreen: LayoutScreen = new LayoutScreen();
+
+  private readonly levels: Levels = new Levels();
+
+  private readonly input: Input = new Input();
+
+  private readonly codeScreen: CodeScreen = new CodeScreen();
+
+  public init(): void {
+    const container = document.querySelector('#container');
+
+    container?.appendChild(this.layoutScreen.createCodeScreenLayout());
+    container?.appendChild(this.levels.start());
+    container?.appendChild(this.input.createInputLayout());
+    container?.appendChild(this.codeScreen.start());
+
+    this.input.checkInputValue();
+  }
+}
