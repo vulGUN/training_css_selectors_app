95d52a1961c7af6753996dbbcfc1d8b3f8c7249c
Max Lihachev <likha.max@gmail.com>
2023-07-01 15:31:38 +0300

    feat: add method which generate css code for game level in codeScreen.ts; add style code for new elements

--- rs-css/src/components/code-screen/codeScreen.ts
+++ rs-css/src/components/code-screen/codeScreen.ts
@@ -1,13 +1,35 @@
 import './codeScreen.css';
+import { GAME_LEVELS } from '../levels/gameLevels';
 
 export class CodeScreen {
-  private readonly codeScreen = 'code-screen';
+  private readonly DIV_SELECTOR = 'div';
 
-  public start(): DocumentFragment {
+  private readonly CODE_SCREEN_SELECTOR: string = 'code-screen';
+
+  private readonly CODE_SCREEN_CONTAINER: HTMLElement = this.createCodeScreenContainer();
+
+  private readonly CODE_SCREEN_BODY_TEXT: HTMLElement = this.createCodeScreenBodyText();
+
+  private createCodeScreenContainer(): HTMLElement {
+    const codeScreenContainer = document.createElement(this.DIV_SELECTOR);
+    codeScreenContainer.classList.add(this.CODE_SCREEN_SELECTOR);
+
+    return codeScreenContainer;
+  }
+
+  private createCodeScreenBodyText(): HTMLElement {
+    const CodeScreenBodyText: HTMLDivElement = document.createElement(this.DIV_SELECTOR);
+    CodeScreenBodyText.classList.add('code-screen__body-text');
+
+    return CodeScreenBodyText;
+  }
+
+  public getcodeScreenContainer(): HTMLElement {
+    return this.CODE_SCREEN_CONTAINER;
+  }
+
+  public createCodeScreenLayout(): DocumentFragment {
     const fragment: DocumentFragment = document.createDocumentFragment();
-
-    const codeScreenContainer = document.createElement('div');
-    codeScreenContainer.classList.add(this.codeScreen);
 
     const CodeScreenHeader: HTMLDivElement = document.createElement('div');
     CodeScreenHeader.classList.add('code-screen__header');
@@ -18,11 +40,31 @@ export class CodeScreen {
     const CodeScreenHeaderRightTitle: HTMLSpanElement = document.createElement('span');
     CodeScreenHeaderRightTitle.innerText = 'index.html';
 
-    CodeScreenHeader.append(CodeScreenHeaderLeftTitle, CodeScreenHeaderRightTitle);
-    codeScreenContainer.append(CodeScreenHeader);
+    const CodeScreenBody: HTMLDivElement = document.createElement('div');
+    CodeScreenBody.classList.add('code-screen__body');
 
-    fragment.append(codeScreenContainer);
+    CodeScreenHeader.append(CodeScreenHeaderLeftTitle, CodeScreenHeaderRightTitle);
+    this.CODE_SCREEN_BODY_TEXT.appendChild(this.generateCodeScreenBodyText());
+
+    CodeScreenBody.append(this.CODE_SCREEN_BODY_TEXT);
+    this.CODE_SCREEN_CONTAINER.append(CodeScreenHeader, CodeScreenBody);
+
+    fragment.append(this.CODE_SCREEN_CONTAINER);
 
     return fragment;
   }
+
+  public generateCodeScreenBodyText(index = 0): DocumentFragment {
+    const CodeScreenBodyTextFragment: DocumentFragment = document.createDocumentFragment();
+
+    GAME_LEVELS[index].code.forEach((item) => {
+      const element = document.createElement(this.DIV_SELECTOR);
+      element.textContent = item.codeText;
+      CodeScreenBodyTextFragment.appendChild(element);
+    });
+
+    return CodeScreenBodyTextFragment;
+  }
+
+  // public changeLevel(): void {}
 }
