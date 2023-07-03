00bfaa2f18d396b2a2be457a0651ebd08d110c75
Max Lihachev <likha.max@gmail.com>
2023-07-03 23:14:25 +0300

    feat: add new game level

--- rs-css/src/components/levels/gameLevels.ts
+++ rs-css/src/components/levels/gameLevels.ts
@@ -1,65 +1,212 @@
-type GameLevelType = {
+export type GameLevelType = {
   title: string;
   menuTitle: string;
   level: string;
   answer: string;
-  code: CodeFargment[];
+  code: CodeFragment[];
 };
 
-type CodeFargment = {
-  codeText: string;
-  children?: CodeFargment[];
+export type CodeFragment = {
+  startTag: string;
+  endTag: string;
+  children?: CodeFragment[];
 };
 
 export const GAME_LEVELS: GameLevelType[] = [
   {
+    title: 'Select the plates',
+    menuTitle: 'A',
+    answer: 'plate',
+    level: '1',
+    code: [
+      {
+        startTag: '<div class="table">',
+        endTag: '</div>',
+        children: [
+          { startTag: '<plate />', endTag: '' },
+          { startTag: '<plate />', endTag: '' },
+        ],
+      },
+    ],
+  },
+  {
     title: 'Select the fancy plate',
     menuTitle: '#ID',
     answer: '#fancy',
-    level: '1',
+    level: '2',
     code: [
-      { codeText: '<div class="table">' },
-      { codeText: '<plate id="fancy" />' },
-      { codeText: '<plate />' },
-      { codeText: '<bento />' },
-      { codeText: '</div>' },
+      {
+        startTag: '<div class="table">',
+        endTag: '</div>',
+        children: [
+          { startTag: '<plate />', endTag: '' },
+          { startTag: '<plate id="fancy" />', endTag: '' },
+          { startTag: '<bento />', endTag: '' },
+        ],
+      },
     ],
   },
   {
     title: 'Select the small apples',
     menuTitle: '.classname',
     answer: '.small',
-    level: '2',
+    level: '3',
     code: [
-      { codeText: '<div class="table">' },
-      { codeText: '<apple />' },
-      { codeText: '<apple class="small" />' },
-      { codeText: '<plate>' },
-      { codeText: '<apple class="small" />' },
-      { codeText: '</plate>' },
-      { codeText: '<plate />' },
-      { codeText: '</div>' },
+      {
+        startTag: '<div class="table">',
+        endTag: '</div>',
+        children: [
+          { startTag: '<apple />', endTag: '' },
+          { startTag: '<apple class="small" />', endTag: '' },
+          {
+            startTag: '<plate>',
+            endTag: '</plate>',
+            children: [{ startTag: '<apple class="small" />', endTag: '' }],
+          },
+          { startTag: '<plate />', endTag: '' },
+        ],
+      },
     ],
   },
   {
     title: 'Select the small oranges',
     menuTitle: 'A.className',
     answer: 'orange.small',
-    level: '3',
+    level: '4',
     code: [
-      { codeText: '<div class="table">' },
-      { codeText: '<apple />' },
-      { codeText: '<apple class="small" />' },
-      { codeText: '<bento>' },
-      { codeText: '<orange class="small" />' },
-      { codeText: '</bento>' },
-      { codeText: '<plate>' },
-      { codeText: '<orange />' },
-      { codeText: '</plate>' },
-      { codeText: '<plate>' },
-      { codeText: '<orange class="small" />' },
-      { codeText: '</plate> }' },
-      { codeText: '</div>' },
+      {
+        startTag: '<div class="table">',
+        endTag: '</div>',
+        children: [
+          { startTag: '<apple />', endTag: '' },
+          { startTag: '<apple class="small" />', endTag: '' },
+          {
+            startTag: '<bento>',
+            endTag: '</bento>',
+            children: [{ startTag: '<orange class="small" />', endTag: '' }],
+          },
+          { startTag: '<plate>', endTag: '</plate>', children: [{ startTag: '<orange />', endTag: '' }] },
+          { startTag: '<plate>', endTag: '</plate>', children: [{ startTag: '<orange class="small" />', endTag: '' }] },
+        ],
+      },
+    ],
+  },
+  {
+    title: 'Select all the things!',
+    menuTitle: '*',
+    answer: '*',
+    level: '5',
+    code: [
+      {
+        startTag: '<div class="table">',
+        endTag: '</div>',
+        children: [
+          { startTag: '<apple />', endTag: '' },
+          { startTag: '<plate>', endTag: '</plate>', children: [{ startTag: '<orange class="small" />', endTag: '' }] },
+          { startTag: '<bento />', endTag: '' },
+          {
+            startTag: '<bento>',
+            endTag: '</bento>',
+            children: [{ startTag: '<orange class="small" />', endTag: '' }],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "Select every apple that's next to a plate",
+    menuTitle: 'A + B',
+    answer: 'plate + .small, plate + apple',
+    level: '6',
+    code: [
+      {
+        startTag: '<div class="table">',
+        endTag: '</div>',
+        children: [
+          { startTag: '<plate>', endTag: '</plate>', children: [{ startTag: '<orange class="small" />', endTag: '' }] },
+          { startTag: '<plate />', endTag: '' },
+          { startTag: '<apple class="small" />', endTag: '' },
+          { startTag: '<plate />', endTag: '' },
+          { startTag: '<apple />', endTag: '' },
+          { startTag: '<apple class="small" />', endTag: '' },
+          { startTag: '<apple class="small" />', endTag: '' },
+        ],
+      },
+    ],
+  },
+  {
+    title: 'Select the 3rd plate',
+    menuTitle: ':nth-child(A)',
+    answer: 'plate:nth-child(3)',
+    level: '7',
+    code: [
+      {
+        startTag: '<div class="table">',
+        endTag: '</div>',
+        children: [
+          { startTag: '<plate />', endTag: '' },
+          { startTag: '<plate />', endTag: '' },
+          { startTag: '<plate />', endTag: '' },
+          { startTag: '<plate class="small" />', endTag: '' },
+        ],
+      },
+    ],
+  },
+  {
+    title: 'Select all even plates',
+    menuTitle: ':nth-of-type(A)',
+    answer: 'plate:nth-of-type(even)',
+    level: '8',
+    code: [
+      {
+        startTag: '<div class="table">',
+        endTag: '</div>',
+        children: [
+          { startTag: '<plate />', endTag: '' },
+          { startTag: '<plate />', endTag: '' },
+          { startTag: '<plate />', endTag: '' },
+          { startTag: '<plate />', endTag: '' },
+          { startTag: '<plate class="small" />', endTag: '' },
+          { startTag: '<plate />', endTag: '' },
+        ],
+      },
+    ],
+  },
+  {
+    title: 'Select the empty bentos',
+    menuTitle: ':empty',
+    answer: 'bento:empty',
+    level: '9',
+    code: [
+      {
+        startTag: '<div class="table">',
+        endTag: '</div>',
+        children: [
+          { startTag: '<bento />', endTag: '' },
+          { startTag: '<bento>', endTag: '</bento>', children: [{ startTag: '<orange class="small" />', endTag: '' }] },
+          { startTag: '<plate />', endTag: '' },
+          { startTag: '<bento />', endTag: '' },
+        ],
+      },
+    ],
+  },
+  {
+    title: 'Select the big apples',
+    menuTitle: ':not(X)',
+    answer: 'apple:not(.small)',
+    level: '10',
+    code: [
+      {
+        startTag: '<div class="table">',
+        endTag: '</div>',
+        children: [
+          { startTag: '<plate>', endTag: '</plate>', children: [{ startTag: '<apple class="small" />', endTag: '' }] },
+          { startTag: '<plate>', endTag: '</plate>', children: [{ startTag: '<apple />', endTag: '' }] },
+          { startTag: '<apple />', endTag: '' },
+          { startTag: '<plate>', endTag: '</plate>', children: [{ startTag: '<orange class="small" />', endTag: '' }] },
+          { startTag: '<bento />', endTag: '' },
+        ],
+      },
     ],
   },
 ];
